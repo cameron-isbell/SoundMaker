c58988203fcb5d0398489120b8cdc720dcc4ae0b
root <root@DESKTOP-NVSUMEG.localdomain>
2022-03-05 15:27:38 -0600

    fixed lag issues, queue now works properly again

--- bot.py
+++ bot.py
@@ -16,11 +16,13 @@ from discord.ext.commands import Bot
 load_dotenv()
 bot = Bot(command_prefix='>')
 
+#TODO: GROUP THESE TOGETHER INTO AN OBJECT
+
 #a dicionary of queues. one for each guild that the bot is playing in
 queue_dict = {}
 
 #a dictionary for each thread associated with each guild
-thrd_dict = {}
+thread_dict = {}
 
 #a dictionary the audio for each guild. 
 audio_dict = {}
@@ -29,14 +31,16 @@ stop = False
 #A thread running to check the queue whenever audio is done playing
 def check_queue(guild):
     global stop
+    global queue_dict
     global audio_dict
 
     while not stop:
         audio = audio_dict[guild.id]
         queue = queue_dict[guild.id]
 
-        while not audio is None and (audio.is_playing() or audio.is_paused) and not stop:
+        if (not audio is None and (audio.is_playing() or audio.is_paused())):
             time.sleep(1)
+            continue
         
         if queue.__len__() > 0:
             info = queue.pop(0)
@@ -54,7 +58,9 @@ async def on_ready():
 #Adds a song, if valid, to the queue to be played
 @bot.command(name='play', aliases=['p'])
 async def play(ctx, *args):
-    global queue
+    global queue_dict
+    global audio_dict
+    global thread_dict
     await ctx.send('Processing Request...')
     url = ""
     #connect the bot to the vc if possible
@@ -73,7 +79,6 @@ async def play(ctx, *args):
         await ctx.send('User is not in a voice channel.')
         return
     
-    #TODO: less lazy solution
     try:
         url = args[0]
     except IndexError:
@@ -91,7 +96,6 @@ async def play(ctx, *args):
 
     #download the data used to play the song
     try:
-        #'format':'bestaudio/best', d
         ydl_info = youtube_dl.YoutubeDL({'format' : 'bestaudio/best', 'noplaylist':'True', 'cachedir' : 'False'})
         with ydl_info:
             info = ydl_info.extract_info(url, download=False)
@@ -109,9 +113,9 @@ async def play(ctx, *args):
         audio_dict[ctx.guild.id] = None
 
     #create a new thread for each guild
-    if not ctx.guild.id in thrd_dict.keys():
-        thrd_dict[ctx.guild.id] = threading.Thread(target=check_queue, args=(ctx.guild,))
-        thrd_dict[ctx.guild.id].start()
+    if not ctx.guild.id in thread_dict.keys():
+        thread_dict[ctx.guild.id] = threading.Thread(target=check_queue, args=(ctx.guild,))
+        thread_dict[ctx.guild.id].start()
 
     embed = discord.Embed(
         title=info['title'],
@@ -128,6 +132,7 @@ async def play(ctx, *args):
 #Remove an item at a specific point in the queue
 @bot.command(name='remove', aliases=['rm'])
 async def remove(ctx, *args):
+    global queue_dict
     idx = int(args[0])
 
     if idx == 0:
@@ -147,6 +152,7 @@ async def remove(ctx, *args):
 #pause the music
 @bot.command(name='pause')
 async def pause(ctx):
+    global audio_dict
     audio = audio_dict[ctx.guild.id]
     if audio is None or not audio.is_playing():
         await ctx.send('No audio playing.')
@@ -157,6 +163,7 @@ async def pause(ctx):
 #Resume audio if it's paused
 @bot.command(name='resume')
 async def resume(ctx):
+    global audio_dict
     audio = audio_dict[ctx.guild.id]
     if audio is None:
         await ctx.send('no audio playing.')
@@ -170,6 +177,7 @@ async def resume(ctx):
 #force bot to leave the voice channel
 @bot.command(name='leave')
 async def leave(ctx):
+    global audio_dict
     audio = audio_dict[ctx.guild.id]
     if not ctx.voice_client:
         await ctx.send('not in a voice channel')
@@ -183,17 +191,20 @@ async def leave(ctx):
 #skip to the next song in the playlist by stopping the current audio
 @bot.command(name='skip', aliases=['s'])
 async def skip(ctx):
+    global audio_dict
     audio = audio_dict[ctx.guild.id]
     if not audio is None and (audio.is_playing() or audio.is_paused()):
         audio.stop()
 
 @bot.command(name='clear')
 async def clear(ctx):
+    global audio_dict
     queue_dict[ctx.guild.id].clear()
     await ctx.send('Queue successfully cleared!')
 
 @bot.command(name='queue')
 async def queue_cmd(ctx):
+    global queue_dict
     if not ctx.guild.id in queue_dict.keys():
         queue_dict[ctx.guild.id] = []
     
