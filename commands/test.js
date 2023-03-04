d6b264dbe49dd62c795045c3f34f63f1ff5c4b08
cisbell5 <47825787+cisbell5@users.noreply.github.com>
2023-03-03 20:00:24 -0600

    now properly responds to commands

--- commands/test.js
+++ commands/test.js
@@ -4,8 +4,7 @@ module.exports = {
     data : new SlashCommandBuilder()
         .setName('test')
         .setDescription('test description'),
-
     async execute(interaction) {
-        await interaction.reply('This command was run by ${interaction.user.username}');
+        await interaction.reply(`This command was run by ${interaction.user.username}`);
     },
 };
