6108557400d8aaaea4aa87aad23fa22457a6eb82
Cameron Isbell <cisbell5@gmail.com>
2023-05-16 01:04:32 -0500

    testing ffmpeg with ytdl

--- commands/play.js
+++ commands/play.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { guildId } = require("./../config.json");
 const { join } = require('node:path');
+const fs = require('fs');
 const { joinVoiceChannel, createAudioPlayer, createAudioResource, getVoiceConnection } = require('@discordjs/voice');
 const ffmpeg = require('fluent-ffmpeg');
 const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path
@@ -10,6 +11,8 @@ const ytdl = require('ytdl-core');
 
 const { create } = require('node:domain');
 const output = require('fluent-ffmpeg/lib/options/output');
+const { findSourceMap } = require('node:module');
+const { downloadFromInfo } = require('ytdl-core');
 
 module.exports = 
 {
@@ -48,31 +51,31 @@ module.exports =
             guildId: interaction.guildId,
             adapterCreator: interaction.guild.voiceAdapterCreator,
         });
-        
-        const filename = join(__dirname, 'fire.mp3');
-
-        ffmpeg(filename)
-        .toFormat('opus')
-        .audioCodec('libopus')
-        .on('error', (err) => {
-            console.log('An error occurred: ' + err.message);
-        })
-        .on('progress', (progress) => {
-            console.log('Processing: ' + progress.targetSize + ' KB converted');
-        })
-        .on('end', () => {
-            console.log('Processing finished !');
-        })
-        .save('./output.opus');//path where you want to save your file
-        
-        //const testconnection = getVoiceConnection(interaction.guildId);
 
         const player = createAudioPlayer();
-        const outputpath = join(__dirname, 'output.opus');
-        let resource = createAudioResource(outputpath);
-
         connection.subscribe(player);
-        player.play(resource);
 
+        //const filename = join(__dirname, 'fire.mp3');
+        let info = await ytdl.getInfo(link);
+
+        ytdl(link, {format : 'mp3', filter : 'audioonly', quality : 'lowest'}).pipe(fs.createWriteStream(join(__dirname, 'audiofiles/downloaded.mp3')))
+        .on('close', () => {
+            console.log('ytdl closed');
+            ffmpeg(join(__dirname, 'audiofiles/downloaded.mp3'))
+            .toFormat('opus')
+            .audioCodec('libopus')
+            .on('error', (err) => {
+                console.log('An error occurred: ' + err.message);
+            })
+            .on('progress', (progress) => {
+                console.log('Processing: ' + progress.targetSize + ' KB converted');
+            })
+            .on('end', () => {
+                console.log('Processing finished !');
+                let resource = createAudioResource(join(__dirname, 'audiofiles/output.opus'));
+                player.play(resource);
+            })
+            .save(join(__dirname, 'audiofiles/output.opus'));
+        });
     }, 
 };
