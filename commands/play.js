b552542359b76601cc78c958214e628b861001a7
Cameron Isbell <cisbell5@gmail.com>
2023-05-16 01:31:23 -0500

    use ytdl-core-discord

--- commands/play.js
+++ commands/play.js
@@ -3,16 +3,14 @@ const { guildId } = require("./../config.json");
 const { join } = require('node:path');
 const fs = require('fs');
 const { joinVoiceChannel, createAudioPlayer, createAudioResource, getVoiceConnection } = require('@discordjs/voice');
+
 const ffmpeg = require('fluent-ffmpeg');
 const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path
 ffmpeg.setFfmpegPath(ffmpegPath)
 
-const ytdl = require('ytdl-core');
-
-const { create } = require('node:domain');
+const ytdl = require('ytdl-core-discord');
 const output = require('fluent-ffmpeg/lib/options/output');
 const { findSourceMap } = require('node:module');
-const { downloadFromInfo } = require('ytdl-core');
 
 module.exports = 
 {
@@ -55,27 +53,12 @@ module.exports =
         const player = createAudioPlayer();
         connection.subscribe(player);
 
-        //const filename = join(__dirname, 'fire.mp3');
         let info = await ytdl.getInfo(link);
+        let formats = ytdl.filterFormats(info.formats, 'audioonly');
+        let format = ytdl.chooseFormat(formats, {audioCodec : 'opus', quality : 'highest'});
 
-        ytdl(link, {format : 'mp3', filter : 'audioonly', quality : 'lowest'}).pipe(fs.createWriteStream(join(__dirname, 'audiofiles/downloaded.mp3')))
-        .on('close', () => {
-            console.log('ytdl closed');
-            ffmpeg(join(__dirname, 'audiofiles/downloaded.mp3'))
-            .toFormat('opus')
-            .audioCodec('libopus')
-            .on('error', (err) => {
-                console.log('An error occurred: ' + err.message);
-            })
-            .on('progress', (progress) => {
-                console.log('Processing: ' + progress.targetSize + ' KB converted');
-            })
-            .on('end', () => {
-                console.log('Processing finished !');
-                let resource = createAudioResource(join(__dirname, 'audiofiles/output.opus'));
-                player.play(resource);
-            })
-            .save(join(__dirname, 'audiofiles/output.opus'));
-        });
+        let resource = createAudioResource(format.url);
+
+        player.play(resource);
     }, 
 };
